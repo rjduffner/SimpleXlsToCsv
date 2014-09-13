d838f82967cd624bd6b86cb65928e4745730a051
rjduffner <rjduffner@gmail.com>
2014-09-12 22:09:15 -0700

    adding test code

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'xlrd',
+    ]
+
+setup(name='simplexlstocsv',
+      version='0.0.1',
+      description='simplexlstocsv',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='xlsx xls csv python',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="simplexlstocsv"
+      )
